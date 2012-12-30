052bd70cadbeb7ac18db0af9073f8df29aee9480
Luze26 <yann.uzel@gmail.com>
2012-12-30 22:25:28 +0100

    Clean

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
-var i = 0;
-
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
 	if (changeInfo.status == "loading") {
-		i++;
-		chrome.browserAction.setBadgeText({text:String(i)});
+		
 	}
 });
-
-chrome.browserAction.setBadgeBackgroundColor({color:[0, 200, 0, 100]});
-chrome.browserAction.setBadgeText({text:"0"});
