f2f0b19e7ef978d251eeb19b91311d250479fc4c
Luze26 <yann.uzel@gmail.com>
2012-12-31 15:16:59 +0100

    Bookmark.js

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
 	if (changeInfo.status == "loading") {
+		chrome.tabs.get(tabId, function(tab) {
+			var callbackProxy = function(bookmarks) {
+				updateBookmark(bookmarks, tab.url);
+			}
+			
+			chrome.storage.sync.get('bookmarks', callbackProxy);
+		});
+		
+		
 		
 	}
 });
+
+function updateBookmark(bookmarks, url) {
+	
+}
--- popup.js
+++ popup.js
@@ -1,36 +1,36 @@
 $(function() {
 	chrome.storage.sync.set({'bookmarks': new Array()});
-	afficher();
 	
-	//Ajout d'une url
+	//Add a bookmark
 	$('#url').change(function() {
-		var bookmarkTreeNodes = chrome.bookmarks.getTree(
-		function(bookmarkTreeNodes) {
-			bookmarkTreeNodes = bookmarkTreeNodes[0].children;
-			
-			//On ajoute le bookmark
-			chrome.bookmarks.create({'parentId': bookmarkTreeNodes[0].id,
-					'title': $('#url').val(),
-					'url': $('#url').val()});
-			
-			//On ajoute au storage
-			chrome.storage.sync.get('bookmarks', function(list) {
-				list.bookmarks[list.bookmarks.length] =  $('#url').val();
-				chrome.storage.sync.set({'bookmarks': list.bookmarks}, afficher());
+		var bookmarkTreeNodes = chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
+				bookmarkTreeNodes = bookmarkTreeNodes[0].children;
+				//Add the bookmark in chrome
+				chrome.bookmarks.create({'parentId': bookmarkTreeNodes[0].id, 'title': $('#url').val(), 'url': $('#url').val()},
+						function(bookmark) {
+							//Add information to the storage
+							
+							var callback = function(list) {
+								list.bookmarks[list.bookmarks.length] =  {'url': $('#url').val(), 'id': bookmark.id};
+								chrome.storage.sync.set({'bookmarks': list.bookmarks});
+							}
+							
+							chrome.storage.sync.get('bookmarks', callback);
+						});
 			});
-		});
 	});
 });
 
-function afficher() {
+/*function display() {
+	//Display bookmarks in the popup
 	$('#bookmarks').empty();
 	chrome.storage.sync.get('bookmarks', function(list) {
 		var i;
 		
 		for(i=0; i<list.bookmarks.length; i++) {
 			var anchor = $('<a>');
-			anchor.attr('href', list.bookmarks[i]);
-			anchor.text(list.bookmarks[i]);
+			anchor.attr('href', list.bookmarks.url[i]);
+			anchor.text(list.bookmarks.url[i]);
 			
 			anchor.click(function() {
 				chrome.tabs.create({url: $(this).context.href});
@@ -40,4 +40,4 @@ function afficher() {
 			$('#bookmarks').append('</br>');
 		}
 	});
-}
+}*/
