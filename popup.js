f871e63340335af1b5b450f8528195a4d1df5bfa
Luze26 <yann.uzel@gmail.com>
2013-01-01 21:03:47 +0100

    Better popup/ Close after add

--- popup.js
+++ popup.js
@@ -1,16 +1,24 @@
 $(function() {
+	chrome.windows.getCurrent(function(w) {
+		chrome.tabs.getSelected(w.id,function (tab){
+			$('#title').val(tab.title);
+			$('#title').select();
+			$('#url').val(tab.url);
+		});
+	});
+  
 	//Add a bookmark
-	$('#url').change(function() {
+	$('#add').click(function() {
 		var bookmarkTreeNodes = chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
 				bookmarkTreeNodes = bookmarkTreeNodes[0].children;
 				//Add the bookmark in chrome
-				chrome.bookmarks.create({'parentId': bookmarkTreeNodes[0].id, 'title': $('#url').val(), 'url': $('#url').val()},
+				chrome.bookmarks.create({'parentId': bookmarkTreeNodes[0].id, 'title': $('#title').val(), 'url': $('#url').val()},
 						function(bookmark) {
 							//Add information to the storage
 							
 							var callback = function(list) {
 								list.bookmarks[list.bookmarks.length] =  {'url': $('#url').val(), 'id': bookmark.id};
-								chrome.storage.sync.set({'bookmarks': list.bookmarks});
+								chrome.storage.sync.set({'bookmarks': list.bookmarks}, window.close());
 							}
 							
 							chrome.storage.sync.get('bookmarks', callback);
